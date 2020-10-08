ea6ab6c74d73051e9c89d6ce29a7772ba9ff2054
Nuung <qlgks1@naver.com>
2020-10-08 16:59:15 +0900

    add linear gradient

--- Weather.js
+++ Weather.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import PropTypes from "prop-types";
+import { LinearGradient } from 'expo-linear-gradient';
+import { MaterialCommunityIcons } from '@expo/vector-icons'; // for use of icons
 
 export default function Weather({ temp }) {
     return (
         <View style={styles.container}>
-            <Text>{temp}</Text>
+            <View style={styles.halfContainer}>
+                <MaterialCommunityIcons size={96} name="weather-lightning-rainy" />
+                <Text style={styles.temp}>{temp}</Text>
+            </View>
+            <View style={styles.halfContainer}>
+
+            </View>
         </View>
+        
     );
 }
 
@@ -28,6 +37,14 @@ Weather.PropTypes = {
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    temp: {
+        fontSize: 42
+    },
+    halfContainer: {
         flex: 1,
         justifyContent: "center",
         alignItems: "center"
--- note/class10~16.md
+++ note/class10~16.md
@@ -20,4 +20,16 @@ https://nomadcoders.co/react-native-fundamentals/lectures/1580
 ###
 
 ## Styling
- - 아이콘을 다루기 전에 expo / vetor-icons를 한 번 살펴보자! []()
+ - 아이콘을 다루기 전에 expo / vetor-icons를 한 번 살펴보자! [expo/vector-icons](https://docs.expo.io/guides/icons/#expovector-icons)
+ - <div style="text-align: center;">
+      <img src="./imgs/class12_img1.png" width = "60%" />
+   </div>
+ - 날씨마다 적절한 icon으로 뿌려주자, 사용법은 아주 간단하다. 컨테이너 안에 아래와 같이 태그 만들어주면 끝
+ ``` <MaterialCommunityIcons size={96} name="weather-lightning-rainy" /> ```
+ - 컨테이너를 2개로 분리하고, 좀 더 이쁘게 스타일링을 해주자! 
+
+ ## Linear Gradient
+ - 스타일링과 레이아웃을 더 이쁘게 하기 위해서 linear-gradient 모듈을 추가로 사용하자 [Link for Docs](https://docs.expo.io/versions/latest/sdk/linear-gradient/)
+ - ```import { LinearGradient } from 'expo-linear-gradient';```
+ - 간단하게 보면, 컴포넌트 위에 또 다른 컴포넌트를 말하는 것이라고 할 수 있다. -> **View 대신** 사용 가능하다. 
+ - 
